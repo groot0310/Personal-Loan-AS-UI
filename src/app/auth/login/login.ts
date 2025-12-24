d879d65684f677b7c89e3de45419325dba453af4
Kunal Baniya <work1.kunalbaniya@gmail.com>
2025-12-24 16:44:15 +0530

    JWT integration

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -8,18 +8,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../authservice/auth-service';
 
 @Component({
   standalone: true,
+  styleUrls: ['./login.component.css'],
   selector: 'app-login',
   imports: [CommonModule, ReactiveFormsModule, RouterLink, FormsModule],
   templateUrl: './login.html',
 })
 export class Login {
-  loginForm: FormGroup;
-showPassword: any;
+   loginForm: FormGroup;
+  showPassword = false;
+  errorMessage = '';
+  loading = false;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router,  private authService: AuthService,) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -29,9 +33,26 @@ showPassword: any;
   submit() {
     if (this.loginForm.invalid) return;
 
-    console.log('Login Data:', this.loginForm.value);
+    this.loading = true;
+    this.errorMessage = '';
 
-    // TEMP navigation (will be replaced by auth + role logic)
-    this.router.navigate(['/user/dashboard']);
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: () => {
+        this.loading = false;
+        this.router.navigate(['/user/dashboard']); // protected route
+      },
+      error: (err) => {
+        this.loading = false;
+           if (err.name === 'TimeoutError') {
+        this.errorMessage = 'Server not responding. Try again.';
+      } else if (err.status === 401) {
+        this.errorMessage = 'Invalid email or password';
+      } else {
+        this.errorMessage = 'Something went wrong';
+      }
+      }
+    });
   }
 }
