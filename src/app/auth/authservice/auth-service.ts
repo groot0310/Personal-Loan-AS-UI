9f8e2417b783c997764ace628a2b51dd1ae2b585
AaDil Haque <aadil10haque12@gmail.com>
2025-12-27 15:14:12 +0530

    apply loan form working

--- src/app/auth/authservice/auth-service.ts
+++ src/app/auth/authservice/auth-service.ts
@@ -4,34 +4,42 @@ import { catchError, Observable, tap, throwError, timeout } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
   private API = 'http://localhost:8080/api/auth';
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
-  return this.http.post<{
-    userProfile: any; token: string 
-}>(`${this.API}/login`, { email, password }).pipe(
-    timeout(5000), // ⏱️ FAIL FAST (5 sec)
-    tap(res => localStorage.setItem('token', res.token)),
-    catchError(err => throwError(() => err))
-  );
-}
+    return this.http
+      .post<{
+        userProfile: any;
+        token: string;
+      }>(`${this.API}/login`, { email, password })
+      .pipe(
+        timeout(5000), // ⏱️ FAIL FAST (5 sec)
+        tap((res) => {
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('email', email);
+          localStorage.setItem('password', password);
+        }),
+        catchError((err) => throwError(() => err))
+      );
+  }
 
   register(data: any): Observable<any> {
     return this.http.post(`${this.API}/register`, data);
   }
 
-//   register(data: any) {
-//   return this.http.post(
-//     'http://localhost:8080/api/auth/register',
-//     data
-//   );
-// } 
+  //   register(data: any) {
+  //   return this.http.post(
+  //     'http://localhost:8080/api/auth/register',
+  //     data
+  //   );
+  // }
 
   logout() {
     localStorage.removeItem('token');
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
   }
 
   isLoggedIn(): boolean {
--- src/app/auth/interceptor/auth-interceptor.ts
+++ src/app/auth/interceptor/auth-interceptor.ts
@@ -7,9 +7,26 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = authService.getToken();
 
   if (token) {
+    if (isTokenExpired(token)) {
+      authService.logout();
+      authService.login(
+        localStorage.getItem('email') || '',
+        localStorage.getItem('password') || ''
+      );
+    }
     req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
+      setHeaders: { Authorization: `Bearer ${token}` },
     });
   }
   return next(req);
 };
+
+function isTokenExpired(token: string): boolean {
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const expirationTime = payload.exp * 1000;
+    return Date.now() < expirationTime;
+  } catch {
+    return true;
+  }
+}
